647b764db55123b69763a9c62f56d28417434405
Edward Menello <edmenello@gmail.com>
2023-07-20 15:14:22 +0200

    Comment in student schema

--- backend/model/student.js
+++ backend/model/student.js
@@ -10,3 +10,18 @@ studentSchema = new Schema( {
 student = mongoose.model('student', studentSchema);
 
 module.exports = student;
+/*
+*TODO
+-Reformat the "name" variable in sessions to be an array of strings rather than one string
+
+	-Right now, the "name" variable is formatted as "["Name", "Name2", "Name3"]"
+
+	-This change allows to loop through when adding a session and updating sessionsAttended variable
+		-sessionsAttended variable to be added to Student Schema
+		-Change name: String --> name: Array
+	
+	-Check to see if any formatting issues in frontend that worked around this issue 
+	-now use the array instead
+
+	-Delete the attendance part of the sessions schema bc it is not used
+*/ 
